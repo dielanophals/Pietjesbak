21bda6ece96e9d874bf2da6423157123e94495ce
Dielan <dielano@hotmail.be>
2018-11-30 19:27:30 +0100

    Switching activities

--- app/src/main/java/be/dielanophals/pietjesbak/MainActivity.java
+++ app/src/main/java/be/dielanophals/pietjesbak/MainActivity.java
@@ -10,37 +10,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final Random random = new Random();
-    private Button rollDices;
-    private ImageView imageView1, imageView2, imageView3;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rollDices  = (Button) findViewById(R.id.rollDices);
-        imageView1 = (ImageView) findViewById(R.id.dice1);
-        imageView2 = (ImageView) findViewById(R.id.dice2);
-        imageView3 = (ImageView) findViewById(R.id.dice3);
 
-        rollDices.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int value1 = randomDiceValue();
-                int value2 = randomDiceValue();
-                int value3 = randomDiceValue();
-
-                int res1 = getResources().getIdentifier("dice" + value1, "drawable", getPackageName());
-                int res2 = getResources().getIdentifier("dice" + value2, "drawable", getPackageName());
-                int res3 = getResources().getIdentifier("dice" + value3, "drawable", getPackageName());
-
-                imageView1.setImageResource(res1);
-                imageView2.setImageResource(res2);
-                imageView3.setImageResource(res3);
-            }
-        });
-    }
-    public static int randomDiceValue(){
-        return random.nextInt(6) + 1;
     }
 }
