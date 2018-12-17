dd37374441974d6c2d341d793e0cca3b5d6df841
Dielan <dielano@hotmail.be>
2018-12-17 01:06:33 +0100

    Added rounds

--- app/src/main/java/be/dielanophals/pietjesbak/Activity2.java
+++ app/src/main/java/be/dielanophals/pietjesbak/Activity2.java
@@ -19,6 +19,8 @@ public class Activity2 extends AppCompatActivity {
 
     Integer throwCounter = 0;
     Boolean turnPlayer1 = true;
+    Integer player1Turns;
+    Integer round = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +28,12 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
 
         Intent intent = getIntent();
-        String string_player1 = intent.getStringExtra(MainActivity.EXTRA_TEXT1);
-        String string_player2 = intent.getStringExtra(MainActivity.EXTRA_TEXT2);
+        final String string_player1 = intent.getStringExtra(MainActivity.EXTRA_TEXT1);
+        final String string_player2 = intent.getStringExtra(MainActivity.EXTRA_TEXT2);
 
         TextView textView1 = (TextView) findViewById(R.id.player1);
         TextView textView2 = (TextView) findViewById(R.id.player2);
-        TextView visual_player = (TextView) findViewById(R.id.visual_player);
+        final TextView visual_player = (TextView) findViewById(R.id.visual_player);
 
         textView1.setText(string_player1);
         textView2.setText(string_player2);
@@ -42,15 +44,19 @@ public class Activity2 extends AppCompatActivity {
         imageView2 = (ImageView) findViewById(R.id.dice2);
         imageView3 = (ImageView) findViewById(R.id.dice3);
 
+        final Button endTurn  = (Button) findViewById(R.id.endTurn);
+
         final CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkbox1);
         final CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkbox2);
         final CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkbox3);
 
+        endTurn.setVisibility(View.INVISIBLE);
         checkBox1.setVisibility(View.INVISIBLE);
         checkBox2.setVisibility(View.INVISIBLE);
         checkBox3.setVisibility(View.INVISIBLE);
 
         final TextView textViewThrowCounter = (TextView) findViewById(R.id.textViewThrowCounter);
+        final TextView textViewround = (TextView) findViewById(R.id.round);
 
         rollDices.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,18 +79,61 @@ public class Activity2 extends AppCompatActivity {
                     imageView3.setImageResource(res3);
                 }
 
+                endTurn.setVisibility(View.VISIBLE);
+
                 throwCounter++;
-                textViewThrowCounter.setText(throwCounter + "/3");
-                if(throwCounter <= 1){
-                    checkBox1.setVisibility(View.VISIBLE);
-                    checkBox2.setVisibility(View.VISIBLE);
-                    checkBox3.setVisibility(View.VISIBLE);
-                }else if (throwCounter == 3){
-                    checkBox1.setVisibility(View.INVISIBLE);
-                    checkBox2.setVisibility(View.INVISIBLE);
-                    checkBox3.setVisibility(View.INVISIBLE);
-                    rollDices.setVisibility(View.INVISIBLE);
+                if(turnPlayer1 == true){
+                    textViewThrowCounter.setText(throwCounter + "/3");
+                    if(throwCounter <= 1){
+                        checkBox1.setVisibility(View.VISIBLE);
+                        checkBox2.setVisibility(View.VISIBLE);
+                        checkBox3.setVisibility(View.VISIBLE);
+                        endTurn.setVisibility(View.VISIBLE);
+                    }else if (throwCounter == 3){
+                        checkBox1.setVisibility(View.INVISIBLE);
+                        checkBox2.setVisibility(View.INVISIBLE);
+                        checkBox3.setVisibility(View.INVISIBLE);
+                        rollDices.setVisibility(View.INVISIBLE);
+                    }
+                }else{
+                    textViewThrowCounter.setText(throwCounter + "/" + player1Turns);
+                    if (throwCounter == player1Turns){
+                        checkBox1.setVisibility(View.INVISIBLE);
+                        checkBox2.setVisibility(View.INVISIBLE);
+                        checkBox3.setVisibility(View.INVISIBLE);
+                        rollDices.setVisibility(View.INVISIBLE);
+                    }
                 }
+            }
+        });
+
+        endTurn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(turnPlayer1 == true){
+                    turnPlayer1 = false;
+                    player1Turns = throwCounter;
+                    textViewThrowCounter.setText("0/" + player1Turns);
+                    visual_player.setText(string_player2);
+                }else{
+                    turnPlayer1 = true;
+                    visual_player.setText(string_player1);
+                    textViewThrowCounter.setText("0/3");
+                    round++;
+                    textViewround.setText("Round " + round);
+                }
+                endTurn.setVisibility(View.INVISIBLE);
+                rollDices.setVisibility(View.VISIBLE);
+                checkBox1.setVisibility(View.VISIBLE);
+                checkBox2.setVisibility(View.VISIBLE);
+                checkBox3.setVisibility(View.VISIBLE);
+
+                throwCounter = 0;
+
+                int image = getResources().getIdentifier("dicestart", "drawable", getPackageName());
+                imageView1.setImageResource(image);
+                imageView2.setImageResource(image);
+                imageView3.setImageResource(image);
             }
         });
 
