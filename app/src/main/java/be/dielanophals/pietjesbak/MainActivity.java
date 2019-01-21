ad5b39d9d8cc5f240b7d99cb014122e72194f5db
AqsaIntizar <43376658+AqsaIntizar@users.noreply.github.com>
2019-01-21 15:46:34 +0100

    Update MainActivity.java

--- app/src/main/java/be/dielanophals/pietjesbak/MainActivity.java
+++ app/src/main/java/be/dielanophals/pietjesbak/MainActivity.java
@@ -10,38 +10,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_TEXT1 = "be.example.application.pietjesbak.EXTRA_TEXT1";
     public static final String EXTRA_TEXT2 = "be.example.application.pietjesbak.EXTRA_TEXT2";
-    private EditText edittekst1;
-    private EditText edittekst2;
-    private Button button;
-    private FirebaseFirestore mFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirestore = FirebaseFirestore.getInstance();
-        button = (Button) findViewById(R.id.start_game);
-        edittekst1 = (EditText) findViewById(R.id.edittext1);
-        edittekst2 = (EditText) findViewById(R.id.edittext2);
+        Button button = (Button) findViewById(R.id.start_game);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity2();
-
             }
         });
     }
@@ -53,17 +37,17 @@ public class MainActivity extends AppCompatActivity {
         EditText player2 = (EditText) findViewById(R.id.edittext2);
         String string_player2 = player2.getText().toString();
 
+        TextView name_check = (TextView) findViewById(R.id.name_check);
 
-        if(string_player1.isEmpty() || string_player2.isEmpty()){// string_player2.isEmpty()){
+        if(string_player1.isEmpty() || string_player2.isEmpty()){
             Toast.makeText(getApplicationContext(),"Gelieve alles in te vullen!", Toast.LENGTH_SHORT).show();
-        }
-        else{
+        }else{
             Intent intent = new Intent(this, Activity2.class);
             intent.putExtra(EXTRA_TEXT1, string_player1);
             intent.putExtra(EXTRA_TEXT2, string_player2);
             startActivity(intent);
-
         }
     }
 
 }
+
